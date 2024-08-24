af6f78d11cf25d0a775484f091c496196e07fae8
bhaveshitankar <bhavesh.itankar@circles.asia>
2024-08-24 23:40:06 +0530

    added extra build

--- frontend/src/lib/apiList.js
+++ frontend/src/lib/apiList.js
@@ -1,4 +1,4 @@
-export const server = "https://main--fresherstorybe.netlify.app/";
+export const server = "https://main--fresherstorybe.netlify.app";
 
 const apiList = {
   login: `${server}/auth/login`,
